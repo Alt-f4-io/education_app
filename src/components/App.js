d890b92b40d137a5f1dc385dcace9ab1d794a02f
Alt-f4-io <stephenaloko7@gmail.com>
2022-03-16 10:49:41 +0000

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,3 @@
-import { AuthProvider } from '../contexts/AuthContext';
-import Signup from './Signup';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Home';
 import Login from './Login';
@@ -9,14 +7,11 @@ export default function App() {
   return (
     <>
       <Router>
-        <AuthProvider>
           <Routes>
             <Route exact path="/" element={<Home/>}/>
-            <Route path="/signup" element={<Signup/>} />
             <Route path="/login" element={<Login/>}/>
             <Route path="/home" element={<Dashboard/>} />
           </Routes>
-        </AuthProvider>
         </Router>
     </>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,41 +1,16 @@
-import React, { useRef, useState } from "react"
-import { useAuth } from "../contexts/AuthContext"
-import { Link, useNavigate } from "react-router-dom"
+import React, { useRef } from "react"
+import { Link } from "react-router-dom"
+import './Login.css'
 
 export default function Login() {
   const emailRef = useRef()
   const passwordRef = useRef()
-  const { login } = useAuth()
-  const [error, setError] = useState("")
-  const [loading, setLoading] = useState(false)
-  const navigate = useNavigate()
-
-  async function handleSubmit(e) {
-    e.preventDefault()
-
-    try {
-      setError("")
-      setLoading(true)
-      await login(emailRef.current.value, passwordRef.current.value)
-      function sendSubmit(){
-        navigate("/home")
-      }
-
-      sendSubmit()
-    } catch {
-      setError("Failed to log in")
-    }
-
-    setLoading(false)
-  }
-
 
     return (
     <div className='body'>
         <div className='card'>
             <h2>Login</h2>
-            {error && <div className="alert-danger"><p>{error}</p></div>}
-            <form onSubmit={handleSubmit}>
+            <form>
                 <div className='form-container'>
                     <div id="email" className="input-container">
                         <label>Email</label>
@@ -46,7 +21,7 @@ export default function Login() {
                         <input className="input-field" type="password" ref={passwordRef} required />
                     </div>
                 </div>
-                <button disabled={loading} className='button' type="submit">Login</button>
+                <button className='button' type="submit">Login</button>
             </form>
 
         </div>
